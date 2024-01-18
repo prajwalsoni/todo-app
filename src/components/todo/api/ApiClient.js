d9a3c892f7c770df6a87e6d5586c4a97ff3f70f1
prajwalsoni <prajwalsonics@gmail.com>
2024-01-18 20:17:34 +0530

    Updated ApiClient

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -2,8 +2,9 @@ import axios from 'axios'
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:8080' 
+        //baseURL: 'http://localhost:8080'  when Yoou have to run the application on localhost:3030
        // baseURL: 'http://03-rest-api-env.eba-p4ruunqb.ap-south-1.elasticbeanstalk.com'
-       //baseURL:'restful-webservices-production.up.railway.app'
+       baseURL:'restful-webservices-production.up.railway.app'
+       //when you have to run your application on vercel and railways
     }
 );
