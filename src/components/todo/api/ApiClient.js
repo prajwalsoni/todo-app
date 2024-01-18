e0a9ad86a787562c3b6b57ec06039407755e9f36
prajwalsoni <prajwalsonics@gmail.com>
2024-01-18 20:14:31 +0530

    Updated ApiClient

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -4,5 +4,6 @@ export const apiClient = axios.create(
     {
         baseURL: 'http://localhost:8080' 
        // baseURL: 'http://03-rest-api-env.eba-p4ruunqb.ap-south-1.elasticbeanstalk.com'
+       //baseURL:'restful-webservices-production.up.railway.app'
     }
 );
